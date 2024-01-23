4ab39c586c25e602333de921584dd29f634bac7a
Jatin Kumar <jk031997@gmail.com>
2024-01-24 04:40:59 +0530

    Revert "Initial commit"
    This reverts commit 9264543345b60d2f8ab95198b5974508f4d6fbf2.

--- src/main/kotlin/com/example/kotlinjpalearn/repository/ProductRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kotlinjpalearn.repository
-
-import com.example.kotlinjpalearn.entity.Product
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ProductRepository : JpaRepository<Product, Long> {
-
-    fun findProductByProductPriceGreaterThan(productPrice: Double): List<Product>
-}
--- src/main/kotlin/com/example/kotlinjpalearn/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.kotlinjpalearn.repository
-
-import com.example.kotlinjpalearn.entity.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User, Long> {
-    fun findUserByUserName(userName: String): User?
-}
