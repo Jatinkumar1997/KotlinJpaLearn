ecc02dd3369670bdf6c4ba858d83193e66356b90
Jatin Kumar <jk031997@gmail.com>
2024-01-24 04:40:36 +0530

    Revert "kotlin basics completed"
    This reverts commit 671570f6741d2be8da60293b73289ad3692bfb8e.

--- src/main/kotlin/com/example/kotlinjpalearn/task1/Car.kt
+++
@@ -1,8 +0,0 @@
-package com.example.kotlinjpalearn.task1
-
-class Car(private val make:String, private val model:String, private val year:Int) {
-
-    fun printCarDetails(){
-        println("Car Details: make: $make , model: $model , year: $year")
-    }
-}
